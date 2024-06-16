1bc7516118a3684069958fd340f53d5a6f37d461
necocen <necocen@gmail.com>
2024-06-16 22:43:59 +0900

    update image url
    なんか更新されなくなった？

--- src/himawari.rs
+++ src/himawari.rs
@@ -5,8 +5,8 @@ mod fetch;
 pub use download::{download_subscription, Progress};
 pub use fetch::fetch_download_info;
 
-const LATEST_JSON_URL: &str = "https://himawari8.nict.go.jp/img/FULL_24h/latest.json";
-const IMAGE_BASE_URL: &str = "https://himawari8.nict.go.jp/img/D531106/2d/550";
+const LATEST_JSON_URL: &str = "https://himawari.asia/img/FULL_24h/latest.json";
+const IMAGE_BASE_URL: &str = "https://himawari.asia/img/D531106/2d/550";
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct DownloadId(DateTime<Utc>);
